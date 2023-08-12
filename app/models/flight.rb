f397fd3cb91a6e70c5783cdc955d29afa6ca05d4
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-12 14:20:30 -0700

    Create Booking Model associated to Flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,5 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
   validates :duration, :departure_datetime, presence: true
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_020115) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_12_211450) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "flight_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -28,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_020115) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
