5806e537002c674d76fa34f51eeb7c0d3d4c662e
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-11 19:41:19 -0700

    Add conditions for blank statements

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,20 @@
 class FlightsController < ApplicationController
   def index
-    @flight = Flight.all
-    @departure_airports = Flight.all.map { |f| [f.departure_airport.code] }
-    @arrival_airports = Flight.all.map { |f| [f.arrival_airport.code] }
+    @departure_airports = Flight.select(:departure_airport_id).distinct.map { |f| [f.departure_airport.code] }
+    @arrival_airports = Flight.select(:arrival_airport_id).distinct.map { |f| [f.arrival_airport.code] }
+    @flight_dates = Flight.order('departure_datetime').map { |f| [f.departure_datetime.strftime("%m/%d/%Y")] }
+
+    dep_air = Airport.find_by(code: params[:departure_airport])
+    arr_air = Airport.find_by(code: params[:arrival_airport])
+    #arr_air = Airport.find_by(code: params[:arrival_airport])
+    if dep_air.nil? && arr_air.nil?
+      @flights = Flight.order(:departure_datetime).limit(50)
+    elsif arr_air.nil?
+      @flights = Flight.where(departure_airport: dep_air.id)
+    elsif dep_air.nil?
+      @flights = Flight.where(arrival_airport: arr_air.id)
+    else
+      @flights = Flight.where(departure_airport: dep_air.id, arrival_airport: arr_air.id)
+    end
   end
 end
