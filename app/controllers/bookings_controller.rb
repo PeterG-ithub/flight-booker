9db9a2c639e6dc11e9817bee9f03ba51c194b543
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-15 10:58:52 -0700

    Add create function with nested form to booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,23 @@ class BookingsController < ApplicationController
   def new
     @num_tickets = params[:bookings][:num_tickets].to_i
     @flight_id = params[:bookings][:flight_id]
+    @booking = Booking.new
+    @num_tickets.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(bookings_param)
+    if @booking.save
+      redirect_to root_path
+    else
+      puts @booking.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookings_param
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
