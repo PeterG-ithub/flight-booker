0eb53b231ee2c342db54200919259a70d4f85f57
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-14 23:14:53 -0700

    Add booking#new view
    Will create sub form that accepts
    name and email to update passenger

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def new
-    @num_tickets = params[:bookings][:num_tickets]
+    @num_tickets = params[:bookings][:num_tickets].to_i
     @flight_id = params[:bookings][:flight_id]
   end
 end
