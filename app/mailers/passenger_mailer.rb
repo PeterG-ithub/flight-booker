23acf3cb7ed3fa7c9d0557c7850bb7d04f8d33ed
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-17 10:18:58 -0700

    add booking_confirmation method to PassengerMailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def booking_confirmation
+    @passenger = params[:passenger]
+    mail(to: @passenger.email, subject: 'Booking created!')
+  end
 end
